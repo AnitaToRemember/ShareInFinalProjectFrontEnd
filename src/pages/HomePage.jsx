145b15c28414c4f6e3e6ee90a8a0dc3883e257c5
AnitaToRemember <best.anac@gmail.com>
2023-09-29 23:38:33 +0200

    <adding comments to the home page>

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,29 +7,41 @@ import ErrorMessage from '../components/ErrorMessage'
 import LinkPost from '../components/LinkPost'
 import Auth from '../components/Auth'
 
+// Home page component
 function HomePage() {
-    const { links, error, loading, addLink, removeLink } = useListLinks()
+    // Retrieve user information from the AuthContext using useContext
     const { user } = useContext(AuthContext)
-    
+
+    // Use the useListLinks hook to fetch, add, and remove links
+    const { links, error, loading, addLink, removeLink } = useListLinks()
+
+    // Render loading component while data is being fetched
     if (loading) return <Loading />
+
+    // Render error message component if there is an error
     if (error) return <ErrorMessage message={error} />
 
-return (
-    <main>
-        <header>
-            <Auth />
-        </header>
-        <section>
-            {user ? <NewLink addLink={addLink} /> : null}
-            
+    // Render the main content of the home page
+    return (
+        <main>
+            {/* Display authentication component in the header */}
+            <header>
+                <Auth />
+            </header>
+            <section>
+                {/* Display the "NewLink" component if a user is authenticated */}
+                {user ? <NewLink addLink={addLink} /> : null}
+                
+                {/* Display the title for the list of latest posts */}
                 <h1>Latest posts</h1>
+
+                {/* Map through the list of links and display each as a "LinkPost" component */}
                 {links.map((link) => (
                     <LinkPost key={link.id} link={link} removeLink={removeLink} />
                 ))}
-        </section>
-    </main>
+            </section>
+        </main>
     )
 }
 
 export default HomePage
-        
